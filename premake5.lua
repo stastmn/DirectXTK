16c1a72bf1e4790d761a32ed45d9370568073bf0
Stas <stastmn@gmail.com>
2021-10-04 15:33:33 +0300

    Switch project to static linked library

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "DirectXTK"
 	kind "StaticLib"
 	language "C++"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
